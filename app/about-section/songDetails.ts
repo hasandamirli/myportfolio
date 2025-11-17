7593102ca40ac9fd0ba1e3b33aaffdbb874ad3eb
Hasan Damirli <hasan.damirli@std.yildiz.edu.tr>
2025-11-17 18:10:58 +0300

    fffff

---
+++ app/about-section/songDetails.ts
@@ -0,0 +1,69 @@
+export type SongProps = {
+  title: string;
+  artist: string;
+  image: string;
+  link: string;
+};
+
+export const songs = [
+  {
+    title: "Napıyorsun Mesela",
+    artist: "BLOK3",
+    image: require(".//../../public/songs/blok3.jpg"),
+    link: "https://open.spotify.com/track/4PA0CfHSeg5yYx7PCyGeMb?si=98bf08e1c1a34c87",
+  },
+  {
+    title: "Kill Bill",
+    artist: "SZA",
+    image: require(".//../../public/songs/killbill.jpg"),
+    link: "https://open.spotify.com/track/3OHfY25tqY28d16oZczHc8?si=06964b686f744956",
+  },
+  {
+    title: "Kaş 2",
+    artist: "Yüzyüzeyken Konuşuruz",
+    image: require(".//../../public/songs/kas2.jpg"),
+    link: "https://open.spotify.com/track/3FNiphAlqUntwM8kzI1jus?si=1547dbd5704a4c07",
+  },
+  {
+    title: "PTSD",
+    artist: "Nemzzz",
+    image: require(".//../../public/songs/ptsd.jpg"),
+    link: "https://open.spotify.com/track/50NJA7Ds65risvAXLspiWK?si=796beea204944bba",
+  },
+  {
+    title: "Daytime",
+    artist: "Jairos, Izzie, Rinblade",
+    image: require(".//../../public/songs/daytime.jpg"),
+    link: "https://open.spotify.com/track/0jU4dsNynRTrrz4w3fKWIZ?si=16f804505d7b4d04",
+  },
+  {
+    title: "Chlorine",
+    artist: "Twenty One Pilots",
+    image: require(".//../../public/songs/chlorine.jpg"),
+    link: "https://open.spotify.com/track/23OXdR7YuUBVWh5hSnYJau?si=44b204e16c7543b0",
+  },
+  {
+    title: "Wildflower",
+    artist: "Billie Eilish",
+    image: require(".//../../public/songs/wildflower.jpg"),
+    link: "https://open.spotify.com/track/3QaPy1KgI7nu9FJEQUgn6h?si=24283a0d4b4a4249",
+  },
+  {
+    title: "Fire Man",
+    artist: "Miyagi & Endspiel",
+    image: require(".//../../public/songs/fireman.jpg"),
+    link: "https://open.spotify.com/track/71hUNAiuvPyzaQGVYQtqlF?si=d68457043db44e7d",
+  },
+  {
+    title: "Mask Off",
+    artist: "Future",
+    image: require(".//../../public/songs/maskoff.jpg"),
+    link: "https://open.spotify.com/track/0VgkVdmE4gld66l8iyGjgx?si=0858e4726ab441fc",
+  },
+  {
+    title: "Bul Beni",
+    artist: "Ezhel",
+    image: require(".//../../public/songs/bulbeni.jpg"),
+    link: "https://open.spotify.com/track/1riBwVUqVsk5RGJ9iLN8cG?si=2c9269652c5e43ae",
+  },
+];
---
+++ app/work-section/projectDetails.ts
@@ -0,0 +1,83 @@
+export type ProjectProps = {
+  id: number;
+  name: string;
+  description: string;
+  technologies: string[];
+  github: string;
+  demo: string;
+  image: string;
+  available: boolean;
+};
+
+export const devProjects = [
+  {
+    id: 0,
+    name: "Servis-M",
+    description:
+      "Corporate website prepared for Servis-M. Developed with focus on products, services, responsive design and fast loading.",
+    technologies: ["HTML", "CSS", "JS"],
+    github: "https://github.com/hasandamirli/servis-m",
+    demo: "https://hasandamirli.github.io/servis-m/",
+    image: require(".//../../public/projects/project-1.jpg"),
+    available: true,
+  },
+  {
+    id: 1,
+    name: "HackXana",
+    description:
+      "A cybersecurity documentary viewing platform designed for Hackxana. It includes a user-friendly interface and activity tracking features.",
+    technologies: ["HTML", "CSS", "JS"],
+    github: "",
+    demo: "https://hackxana.com",
+    image: require(".//../../public/projects/project-2.jpg"),
+    available: true,
+  },
+];
+
+export const designProjects = [
+  {
+    id: 1,
+    name: "Hebron Statup Lab Website",
+    description:
+      "SkyWatch is a convenient and user-friendly tool that allows you to quickly and easily check the current.",
+    technologies: ["UX Research", "UI Design", "Prototyping"],
+    github: "",
+    demo: "",
+    image: "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Fhsl.webp&w=1920&q=75",
+    available: false,
+  },
+  {
+    id: 2,
+    name: "RAGS Scrubs Website",
+    description:
+      "An image generator website that allows users to generate, combine, and download images.",
+    technologies: ["UX Research", "UI Design", "Prototyping"],
+    github: "",
+    demo: "",
+    image: "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Frags.webp&w=1920&q=75",
+    available: false,
+  },
+  {
+    id: 3,
+    name: "Crown Branding Agency Website",
+    description:
+      "A website that reduces the length of your URL using Bit.ly's API",
+    technologies: ["UX Research", "UI Design", "Prototyping"],
+    github: "",
+    demo: "",
+    image:
+      "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Fcrown.webp&w=1920&q=75",
+    available: false,
+  },
+  {
+    id: 4,
+    name: "Titi Mobile App",
+    description:
+      "TMTM helps you find people who are headed to the same location as you, so you can share a ride and split the cost with them.",
+    technologies: ["UX Research", "UI Design", "Prototyping"],
+    github: "",
+    demo: "",
+    image: "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Ftiti.webp&w=1920&q=75",
+    available: false,
+  },
+];
